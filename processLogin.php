f5c0a46aeac7a914c1dca4302e618fd5fd34e007
Kent Huang <kenttttt0@gmail.com>
2017-04-15 21:58:21 -0700

    add 3 times in a row validation

--- processLogin.php
+++ processLogin.php
@@ -8,6 +8,7 @@
 <?php
 
 require_once "commonHead.php";
+require_once 'PHPMailer-master/PHPMailerAutoload.php';
 
 session_start();
 
@@ -42,6 +43,9 @@ if($numOfuser==1&&strcmp($row[0]['password'], $pass)==0&&$row[0]['isactive'] == 
     $value = session_id();
     setcookie("id", $value, time()+1200);
 
+    $resume = "UPDATE users SET attackTime = 0 WHERE email = '$email'";
+    mysqli_query($db,$resume);
+
     header("Location:notes.php");
     die();
     //not activated
@@ -62,7 +66,71 @@ if($numOfuser==1&&strcmp($row[0]['password'], $pass)==0&&$row[0]['isactive'] == 
 
 <br>Then you can <a href='index.php'>log in</a>. <span style='color:red;text-decoration:blink'>Alternatively, you can finish signing up <a href='index.php?r=$confirmcode&e=$email'></span>now</a>.";
 } else{
+
+    if($row) {
+        $getAttack = $row[0]['attackTime'];
+        //echo $getAttack;
+        if(++$getAttack == 3) {
+            $resume = "UPDATE users SET attackTime = 0 WHERE email = '$email'";
+            mysqli_query($db,$resume);
+            //TODO Send email and change password
+            $newPass = bin2hex(random_bytes(2));
+
+            sendemail($email,ROOTDOMAIN,$newPass);
+            $hashPass = hash("sha256", $newPass);
+            $qResetPass = "UPDATE users SET password = '$hashPass' WHERE email = '$email'";
+            $resultReset = mysqli_query($db,$qResetPass) or die(mysqli_error($db));
+
+            echo "Account $email locked.<br>";
+            echo "Use the password sent to your email to activate and login.Try <a href='index.php'>here</a><br>";
+
+
+        } else {
+
+            $incr = "UPDATE users SET attackTime = $getAttack WHERE email = '$email'";
+            mysqli_query($db,$incr);
+        }
+
+    }
+
+
+
+
     echo " Error logging in. Try <a href='index.php'>logging in</a> again or <a href='register2.php'>register</a> for a new account";
+}
+
+function sendemail($toemail,$mysiteurl,$newPass) {
+    date_default_timezone_set('America/Vancouver');
+    $today = date("D M j G:i:s T Y");
+    $mail = new PHPMailer(); // create a new object
+    $mail->IsSMTP(); // enable SMTP
+    //$mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
+    $mail->SMTPAuth = true; // authentication enabled
+    $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
+    $mail->Host = "smtp.gmail.com";
+    $mail->Port = 465; // or 587
+    $mail->IsHTML(true);
+    $mail->Username = "kentsnotetome@gmail.com";
+    $mail->Password = "1234567asd";
+    $mail->SetFrom("kentsnotetome@gmail.com");
+    $mail->Subject = "break-in attempt was detected for $mysiteurl";
+    $mail->Body = "Password change for $toemail
+    <br>
+    <br>
+    <br>
+    From kentsnotetome@gmail.com on $today: 
+    <br><br>
+
+    YOUR NEW PASSWORD IS $newPass. Please keep this email or write this down.
+    To log in again, please click this link: $mysiteurl";
+    $mail->AddAddress("$toemail");
+    $mail->Send();
+
+    /*    if(!$mail->Send()) {
+            echo "Mailer Error: " . $mail->ErrorInfo;
+        } else {
+            echo "Message has been sent";
+        }*/
 }
 
 
